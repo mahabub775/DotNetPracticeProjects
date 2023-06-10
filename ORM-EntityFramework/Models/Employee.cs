96f00c4adf433106ecc4587fbee4ff3ef4fe0da2
mahabub775 <mahabub775@gmail.com>
2023-06-10 12:46:25 +0600

    basic-functionality-Complete

---
+++ ORM-EntityFramework/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ORM_EntityFramework.Models
+{
+	public class Employee
+	{
+		public Employee()
+		{
+			Id = 0;
+			Name = "";
+			Salary = 0;
+			JoinDate = DateTime.Today;
+			ErrorMessage = string.Empty;
+		}
+		public string Name { get; set; }
+		public int Id { get; set; }
+		public decimal Salary { get; set; }
+		public DateTime JoinDate { get; set; }
+
+		[NotMapped]
+		public string ErrorMessage { get; set; }
+	}
+}
---
+++ ORM-EntityFramework/RepositoryServices/IEmployeeService.cs
@@ -0,0 +1,11 @@
+﻿namespace ORM_EntityFramework.RepositoryServices
+{
+	public interface IEmployeeService
+	{
+		List<Employee> Gets();
+		Employee Get(int id);
+		Employee Create(Employee oEmployee);
+		Employee Update(int id, Employee oEmployee);
+		string Delete(int id);
+	}
+}
