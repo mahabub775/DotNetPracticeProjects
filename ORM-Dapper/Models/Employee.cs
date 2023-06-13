ebdc102b8b9707e240a248bb69cc6eee8b49cf43
mahabub775 <mahabub775@gmail.com>
2023-06-13 08:30:07 +0600

    project-initalize-Both_ORM EF & Dapper

---
+++ ORM-Dapper/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ORM_Dapper.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public decimal? Salary { get; set; }
+
+    public DateTime? JoinDate { get; set; }
+}
---
+++ ORM-Dapper/RepositoryServices/IEmployeeService.cs
@@ -0,0 +1,11 @@
+﻿namespace ORM_Dapper.RepositoryServices
+{
+	public interface IEmployeeService
+	{
+		Employee Add(Employee employee);
+		string Delete(int id);
+		List<Employee> GetAll();
+		Employee Get(int id);
+			
+	}
+}
