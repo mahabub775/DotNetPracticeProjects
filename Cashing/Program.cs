ee8da1f4d5909252ebcbda414a92505a557c1b02
mahabub775 <mahabub775@gmail.com>
2023-06-18 08:31:52 +0600

    inital-project for Cashing

---
+++ Cashing/Program.cs
@@ -0,0 +1,29 @@
+global using Cashing.Models;
+global using Cashing.RepositoryServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
+builder.Services.AddDbContext<CashingProjectContext>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
---
+++ Cashing/RepositoryServices/EmployeeService.cs
@@ -0,0 +1,60 @@
+﻿using Cashing.Models;
+
+namespace Cashing.RepositoryServices
+{
+	public class EmployeeService : IEmployeeService
+	{
+		#region Declaration
+
+		private readonly CashingProjectContext _DNPPContext; //dotNetPracticeProjectContext
+
+		#endregion
+		public EmployeeService(CashingProjectContext cashingProjectContext)
+		{
+			_DNPPContext = cashingProjectContext;
+		}
+		#region Interface implementation
+		public Employee Create(Employee oEmployee)
+		{
+			_DNPPContext.Employees.Add(oEmployee);
+			_DNPPContext.SaveChanges();
+			return oEmployee;
+		}
+
+		public string Delete(int id)
+		{
+			var ofindOb = _DNPPContext.Employees.Where(x => x.Id == id).FirstOrDefault();
+			_DNPPContext.Employees.Remove(ofindOb);
+			_DNPPContext.SaveChanges();
+			return "Deleted";
+		}
+
+		public Employee Get(int id)
+		{
+			return _DNPPContext.Employees.Where(x => x.Id==id).FirstOrDefault();
+		}
+
+		public List<Employee> Gets()
+		{
+			//return _oEmployees;
+			return _DNPPContext.Employees.ToList();
+		}
+
+		public Employee Update(int id, Employee oEmployee)
+		{
+			var ofinEmployee= _DNPPContext.Employees.Where(x => x.Id == id).FirstOrDefault();
+			if (ofinEmployee != null)
+			{
+				ofinEmployee.Name = oEmployee.Name;
+				ofinEmployee.Salary = oEmployee.Salary;
+				
+				ofinEmployee.JoinDate = oEmployee.JoinDate;
+			}
+			_DNPPContext.SaveChanges();
+			return ofinEmployee;
+		}
+	
+
+		#endregion
+	}
+}
